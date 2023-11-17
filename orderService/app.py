678b20327f8baa451c9b23949678bedf0cd282e0
bastien707 <bastien.facqueur@efrei.net>
2023-11-17 21:10:05 +0100

    feat: new /order endpoint

--- orderService/app.py
+++ orderService/app.py
@@ -1,8 +1,23 @@
 from flask import Flask
-from routes.hello_route import hello
+from sqlalchemy import create_engine, URL
+from sqlalchemy.orm import sessionmaker
+from routes.order_route import order_bp
+import sqlalchemy as db
 
-app = Flask(__name__)
-app.register_blueprint(hello)
+
+def init_app():
+    app = Flask(__name__)
+
+    engine = db.create_engine(
+        'mysql+mysqlconnector://root:abc@localhost:3307/phoneStore')
+    connection = engine.connect()
+    session = sessionmaker(bind=engine)
+    app.session = session()
+    # Register Blueprints
+    app.register_blueprint(order_bp)
+
+    return app
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    init_app().run(debug=True)
--- orderService/routes/hello_route.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, Blueprint
-
-hello = Blueprint('hello', __name__)
-
-
-@hello.route('/')
-def hello_world():
-    return 'Hello, World!'
