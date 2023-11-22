058580e243125a1ccc968001987680b08730e141
bastien707 <bastien.facqueur@efrei.net>
2023-11-22 16:56:22 +0100

    fix: merge conflict

--- database/imageCreationAndRun.sh
+++ database/imageCreationAndRun.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 docker build -t db_service:latest .
 docker run --network=phoneStoreNetwork -p 3307:3306 -d --name db_container db_service
-=======
-docker build -t phone_store:latest .
-docker run -p 3307:3306 -d phone_store
->>>>>>> dev_productService
--- orderService/imageCreationAndRun.sh
+++ orderService/imageCreationAndRun.sh
@@ -1,3 +1,2 @@
 docker build -t order_service .
 docker run --network=phoneStoreNetwork -p 8080:5000 -d --name order_container order_service
-
