37c7d6b1f5fb1321fd096b3d1b4c8e29fc69bad3
joel-alt <joel.gabbai@efrei.net>
2023-11-22 19:35:29 +0100

    fix bugs

--- orderService/imageCreationAndRun.sh
+++ orderService/imageCreationAndRun.sh
@@ -1,2 +1,2 @@
 docker build -t order_service .
-docker run --network=phoneStoreNetwork -p 8080:5000 -d --name order_container order_service
+docker run --network=phoneStoreNetwork -p 5000:5000 -d --name order_container order_service
