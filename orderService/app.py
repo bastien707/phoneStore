cd5b2890a81df0b8255612b2fe5933620b297cc8
bastien707 <bastien.facqueur@efrei.net>
2023-12-06 16:56:56 +0100

    feat: allow CORS

--- orderService/app.py
+++ orderService/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from routes.order_route import order_bp
-
+from flask_cors import CORS
 
 def init_app():
     app = Flask(__name__)
+    CORS(app)
     engine = create_engine(
         'mysql+mysqlconnector://root:abc@mysql-service:3306/phoneStore')
     engine.connect()  # Ensure connection is established
