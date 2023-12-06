52cf0c462322f78deb272c1ed95044bc7ee6b7a5
joel-alt <joel.gabbai@efrei.net>
2023-12-06 14:53:05 +0100

    update java service

--- productService/image/imageCreationAndRun.sh
+++ productService/image/imageCreationAndRun.sh
@@ -1,2 +1,3 @@
 docker build -t joel22222/phone_image:latest .
-docker run -p 8080:8080 --network=phoneStoreNetwork -d --name phone-container joel22222/phone_image
+docker push joel22222/phone_image:latest
+#docker run -p 8080:8080 --network=phoneStoreNetwork -d --name phone-container joel22222/phone_image
